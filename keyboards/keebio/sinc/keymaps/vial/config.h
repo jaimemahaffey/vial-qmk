29891f0978d679281fe1019b67d2b039f9fe5192
Jaime Mahaffey <jaimemahaffey@gmail.com>
2023-01-05 17:35:50 -0800

    Initial Commit
    Added support for vial in my Keebio Sinc Rev 2 configuration

---
+++ keyboards/keebio/sinc/keymaps/vial/config.h
@@ -0,0 +1,13 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0x0C, 0xBF, 0x6C, 0x83, 0xE7, 0xB8, 0x10, 0xE7}
+// #define VIAL_UNLOCK_COMBO_ROWS {0, 2}
+// #define VIAL_UNLOCK_COMBO_COLS {0, 13}
+
+#define NO_MUSIC_MODE
+#define DYNAMIC_KEYMAP_LAYER_COUNT 3
+#define VIAL_TAP_DANCE_ENTRIES 3
+#undef RGBLIGHT_ANIMATIONS
+
