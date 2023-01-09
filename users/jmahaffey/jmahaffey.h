aef69ca0fa88b30e6a5a0113359a54a3af7447f4
Jaime Mahaffey <jaimemahaffey@gmail.com>
2023-01-09 10:15:28 -0800

    updates to keymap and features

---
+++ users/jmahaffey/jmahaffey.h
@@ -0,0 +1,30 @@
+/* Copyright 2022 Jaime Mahaffey <jaime.mahaffey@gmail.com>
+  *
+  * This program is free software: you can redistribute it and/or modify
+  * it under the terms of the GNU General Public License as published by
+  * the Free Software Foundation, either version 2 of the License, or
+  * (at your option) any later version.
+  *
+  * This program is distributed in the hope that it will be useful,
+  * but WITHOUT ANY WARRANTY; without even the implied warranty of
+  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  * GNU General Public License for more details.
+  *
+  * You should have received a copy of the GNU General Public License
+  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+  */
+#pragma once
+
+#include QMK_KEYBOARD_H
+
+#include "wrappers.h"
+
+enum layers {
+    _QWERTY,
+    _LOWER,
+    _RAISE
+};
+
+
+#define KC_LOWR MO(_LOWER)
+#define KC_RASE MO(_RAISE)
