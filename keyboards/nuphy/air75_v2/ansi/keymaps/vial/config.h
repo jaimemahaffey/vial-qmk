20159741fd6f9983df670c85bd24239af13fc463
Jaime Mahaffey <jaimemahaffey@gmail.com>
2025-02-08 13:52:57 -0800

    Add Nuphy Air75 v2

---
+++ keyboards/nuphy/air75_v2/ansi/keymaps/vial/config.h
@@ -0,0 +1,7 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0xE3, 0xC9, 0x26, 0x28, 0x5A, 0x85, 0xDD, 0x12}
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 3 }
+#define VIAL_UNLOCK_COMBO_COLS { 0, 13 }
